65d69da581d0e5138a794224374ba80317ca93e8
Cikal Arifin <cikal.123140109@student.itera.ac.id>
2026-05-31 16:53:53 +0700

    Update ApiConfig.android.kt

--- composeApp/src/androidMain/kotlin/com/example/todomaster/core/network/ApiConfig.android.kt
+++ composeApp/src/androidMain/kotlin/com/example/todomaster/core/network/ApiConfig.android.kt
@@ -14,5 +14,5 @@ import com.example.todomaster.BuildConfig
  * 3. Build project (API key akan di-inject ke BuildConfig)
  */
 actual object ApiConfig {
-    actual val geminiApiKey: String = "AIzaSyBsdLQET_Mc8H9xt4owYZQcsNMOurnoyG4"
+    actual val geminiApiKey: String = "MASUKKAN_API_KEY_DISINI"
 }
